a695b05dbb87af1f2de24c63da9597e775e6ad4a
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2025-12-21 14:18:28 -0600

    move to js and clean home page

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Schedule Builder</h1>
+      <p>Let's build an office scheduling tool!</p>
+    </div>
+  );
+}
